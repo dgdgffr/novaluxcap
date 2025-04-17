55f586d5955fe6f11e1604e846947a3419723d6e
dgdgffr <wus79849@gmail.com>
2025-04-17 16:10:33 +0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <h1>輝：這是一個專屬網站入口</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
